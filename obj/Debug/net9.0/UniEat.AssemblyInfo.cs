aeae2f588c7e33386dc2bc2bab06cebeaf8a79bc
noahhhsss <noaqimalrawahi@gmail.com>
2026-03-16 00:47:09 +0800

    Profile

--- obj/Debug/net9.0/UniEat.AssemblyInfo.cs
+++ obj/Debug/net9.0/UniEat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniEat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b13b92fdcd40fe57aeaeb538df7fdbbc51eb8ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniEat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniEat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
