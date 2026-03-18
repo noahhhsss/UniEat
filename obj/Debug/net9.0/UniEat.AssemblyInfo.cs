fd41f494acf7971e05ca66ade46d154a590820e3
noahhhsss <noaqimalrawahi@gmail.com>
2026-03-18 20:04:41 +0800

    try

--- obj/Debug/net9.0/UniEat.AssemblyInfo.cs
+++ obj/Debug/net9.0/UniEat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniEat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b20677707aaa62d747f406910d08bb2bafd822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cffa0590dcc120193187f6dd9f219cfcf381a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniEat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniEat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
